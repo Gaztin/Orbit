10f1a3ae4e8d9ca636814d2940dc1b534af9f93b
Sebastian Kylander <sebastian@kylander.me>
2018-07-29 16:36:09 +0200

    Update benchmarks

--- src/samples/02/main.cpp
+++ src/samples/02/main.cpp
@@ -1,40 +1,103 @@
-#include <sstream>
+#include <map>
 
 #include <time.h>
 
 #include <orbit/core/log.h>
+#include <orbit/core/stringf.h>
 #include <orbit/core/window.h>
 #include <orbit/core/utility.h>
 #include <orbit/graphics/render_context.h>
 
-void recreate_contexts(orb::graphics_api api, size_t count)
+#if defined(ORB_OS_WINDOWS)
+#include <windows.h>
+#endif
+
+class scoped_benchmark
 {
-	orb::window w(1024, 1024);
-	w.show();
-	clock_t c1 = clock();
+public:
+	scoped_benchmark(const std::string& verb)
+		: m_verb(verb)
+	{
+		timespec_get(&m_startTimeSpec, TIME_UTC);
+	}
+
+	~scoped_benchmark()
+	{
+		timespec currTimeSpec;
+		timespec_get(&currTimeSpec, TIME_UTC);
+		const double dur = (currTimeSpec.tv_sec - m_startTimeSpec.tv_sec) + 0.000000001 * (currTimeSpec.tv_nsec - m_startTimeSpec.tv_nsec);
+		orb::log_info(orb::stringf("[%.7fs] to %s.", dur, m_verb.c_str()));
+	}
+
+private:
+	std::string m_verb;
+	timespec m_startTimeSpec;
+};
+
+const std::map<orb::graphics_api, std::string> api_names =
+{
+	{ orb::graphics_api::OpenGL, "OpenGL" },
+	{ orb::graphics_api::D3D11, "Direct3D 11" },
+};
+
+static void prepare()
+{
+#if defined(ORB_OS_WINDOWS)
+	HDC hdc = GetDC(nullptr);
+	DescribePixelFormat(hdc, 0, 0, nullptr);
+	ReleaseDC(nullptr, hdc);
+#endif
+}
+
+void bench_window_create(size_t count)
+{
+	scoped_benchmark bench(orb::stringf("create %d windows", count));
 	for (size_t i = 0; i < count; ++i)
 	{
-		const float fraction = (orb::cast<float>(i) / count);
-		volatile orb::render_context rc(w, api);
+		orb::window w(1024, 1024);
 	}
-	clock_t c2 = clock();
+}
 
-	std::stringstream ss;
-	ss << "Created and destroyed " << count << " ";
-	switch (api)
+void bench_context_create(orb::graphics_api api, size_t count)
+{
+	orb::window w(1024, 1024);
+	scoped_benchmark bench(orb::stringf("create %d %s contexts", count, api_names.at(api).c_str()));
+	for (size_t i = 0; i < count; ++i)
 	{
-		case orb::graphics_api::OpenGL: ss << "OpenGL"; break;
-		case orb::graphics_api::D3D11: ss << "Direct3D 11"; break;
-		default: ss << "<Unknown API>";
+		orb::render_context(w, api);
 	}
-	ss << " contexts in " << 0.001f * orb::cast<float>(c2 - c1) << " seconds.\n";
-	orb::log_info(ss.str());
+}
+
+void bench_context_clear(orb::graphics_api api, size_t count)
+{
+	orb::window w(1024, 1024);
+	orb::render_context rc(w, api);
+	rc.make_current(w);
+	rc.set_clear_color(1.0f, 0.0f, 1.0f);
+	scoped_benchmark bench(orb::stringf("clear %s context %d times", api_names.at(api).c_str(), count));
+	for (size_t i = 0; i < count; ++i)
+	{
+		rc.clear(orb::buffer_mask::Color);
+	}
 }
 
 int main(int /*argc*/, char* /*argv*/[])
 {
-	recreate_contexts(orb::graphics_api::OpenGL, 100);
-	recreate_contexts(orb::graphics_api::D3D11, 100);
+	prepare();
+
+	/* Window benchmarks. */
+	orb::log_info("[__Window__]");
+	bench_window_create(5);
+
+	/* OpenGL benchmarks. */
+	orb::log_info("[__OpenGL__]");
+	bench_context_create(orb::graphics_api::OpenGL, 10);
+	bench_context_clear(orb::graphics_api::OpenGL, 1000);
+
+	/* Direct3D 11 benchmarks. */
+	orb::log_info("[__Direct3D_11__]");
+	bench_context_create(orb::graphics_api::D3D11, 10);
+	bench_context_clear(orb::graphics_api::D3D11, 1000);
 
 #if !defined(ORB_OS_ANDROID)
 	getc(stdin);
