d01629149ad8c5577ae814c2a7433ac2f90b773a
Sebastian Kylander <sebastian@kylander.me>
2018-08-03 00:24:47 +0200

    Add Event Dispatcher class

---
+++ src/orbit/core/event_dispatcher.h
@@ -0,0 +1,73 @@
+/*
+* Copyright (c) 2018 Sebastian Kylander http://gaztin.com/
+*
+* This software is provided 'as-is', without any express or implied warranty. In no event will
+* the authors be held liable for any damages arising from the use of this software.
+*
+* Permission is granted to anyone to use this software for any purpose, including commercial
+* applications, and to alter it and redistribute it freely, subject to the following restrictions:
+*
+* 1. The origin of this software must not be misrepresented; you must not claim that you wrote the
+*    original software. If you use this software in a product, an acknowledgment in the product
+*    documentation would be appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be misrepresented as
+*    being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+#pragma once
+#include <functional>
+#include <utility>
+#include <vector>
+
+#include "orbit/core.h"
+
+namespace orb
+{
+
+template<typename EventType>
+class event_dispatcher
+{
+public:
+	struct subscriber_t
+	{
+		uint64_t id;
+		std::function<void(const EventType&)> functor;
+	};
+
+	event_dispatcher() = default;
+
+	template<typename Functor>
+	uint64_t subscribe(Functor&& functor)
+	{
+		static uint64_t uniqueId = 0;
+		m_subscribers.push_back({++uniqueId, std::forward<Functor>(functor)});
+		return uniqueId;
+	}
+
+	void unsubscribe(uint64_t id)
+	{
+		for (auto it = m_subscribers.begin(); it != m_subscribers.end(); ++it)
+		{
+			if (it->id == id)
+			{
+				m_subscribers.erase(it);
+				return;
+			}
+		}
+	}
+
+protected:
+	void send_event(const EventType& e)
+	{
+		for (subscriber_t& subscriber : m_subscribers)
+		{
+			subscriber.functor(e);
+		}
+	}
+
+private:
+	std::vector<subscriber_t> m_subscribers;
+};
+
+}
