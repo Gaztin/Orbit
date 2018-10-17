52a5d9b42a1c7717b951170e3d6bc4088990e9b3
Sebastian Kylander <sebastian@kylander.me>
2018-10-17 21:21:50 +0200

    Add gl functions

---
+++ src/orbit/graphics/gl.cpp
@@ -0,0 +1,89 @@
+/*
+* Copyright (c) 2018 Sebastian Kylander http://gaztin.com/
+*
+* This software is provided 'as-is', without any express or implied warranty. In no event will
+* the authors be held liable for any damages arising from the use of this software.
+*
+* Permission is granted to anyone to use this software for any purpose, including commercial
+* applications, and to alter it and redistribute it freely, subject to the following restrictions:
+*
+* 1. The origin of this software must not be misrepresented; you must not claim that you wrote the
+*    original software. If you use this software in a product, an acknowledgment in the product
+*    documentation would be appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be misrepresented as
+*    being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include "gl.h"
+
+#include <vector>
+
+namespace orb
+{
+namespace gl
+{
+
+static struct functions_t
+{
+	~functions_t()
+	{
+		for (auto pair : pairs)
+			*pair.first = nullptr;
+	};
+
+	std::vector<std::pair<void**, std::string_view>> pairs;
+
+} s_functions;
+
+template<typename Ret, typename... Args>
+static void register_function(Ret(** funPtrAddr)(Args...), std::string_view name)
+{
+	s_functions.pairs.emplace_back(reinterpret_cast<void**>(funPtrAddr), name);
+}
+
+#define REGISTER(X, SYM) decltype(X) X = (register_function(&X, SYM), nullptr)
+
+REGISTER(bind_buffer, "glBindBuffer");
+REGISTER(buffer_data, "glBufferData");
+REGISTER(buffer_sub_data, "glBufferSubData");
+REGISTER(delete_buffers, "glDeleteBuffers");
+REGISTER(disable_vertex_attrib_array, "glDisableVertexAttribArray");
+REGISTER(draw_arrays, "glDrawArrays");
+REGISTER(draw_elements, "glDrawElements");
+REGISTER(enable_vertex_attrib_array, "glEnableVertexAttribArray");
+REGISTER(gen_buffers, "glGenBuffers");
+REGISTER(get_buffer_parameteriv, "glGetBufferParameteriv");
+REGISTER(get_buffer_pointerv, "glBufferPointerv");
+REGISTER(get_vertex_attribdv, "glGetVertexAttribdv");
+REGISTER(get_vertex_attribfv, "glGetVertexAttribfv");
+REGISTER(get_vertex_attribiv, "glGetVertexAttribiv");
+REGISTER(get_vertex_attrib_pointerv, "glGetVertexAttribPointerv");
+REGISTER(is_buffer, "glIsBuffer");
+REGISTER(vertex_attrib1f, "glVertexAttrib1f");
+REGISTER(vertex_attrib2f, "glVertexAttrib2f");
+REGISTER(vertex_attrib3f, "glVertexAttrib3f");
+REGISTER(vertex_attrib4f, "glVertexAttrib4f");
+REGISTER(vertex_attrib_pointer, "glVertexAttribPointer");
+
+namespace this_platform
+{
+
+void* get_proc_address(std::string_view name)
+{
+#if defined(ORB_OS_WINDOWS)
+	return static_cast<void*>(wglGetProcAddress(name.data()));
+#elif defined(ORB_OS_LINUX)
+	return static_cast<void*>(glXGetProcAddress(reinterpret_cast<const GLubyte*>(name.data())));
+#elif defined(ORB_OS_MACOS)
+	static void* lib = dlopen("/System/Library/Frameworks/OpenGL.framework/Versions/Current/OpenGL", RTLD_LAZY);
+	return dlsym(lib, name.data());
+#elif defined(ORB_OS_ANDROID)
+	return static_cast<void*>(eglGetProcAddress(name.data()));
+#endif
+}
+
+}
+
+}
+}
---
+++ src/orbit/graphics/gl.h
@@ -0,0 +1,161 @@
+/*
+* Copyright (c) 2018 Sebastian Kylander http://gaztin.com/
+*
+* This software is provided 'as-is', without any express or implied warranty. In no event will
+* the authors be held liable for any damages arising from the use of this software.
+*
+* Permission is granted to anyone to use this software for any purpose, including commercial
+* applications, and to alter it and redistribute it freely, subject to the following restrictions:
+*
+* 1. The origin of this software must not be misrepresented; you must not claim that you wrote the
+*    original software. If you use this software in a product, an acknowledgment in the product
+*    documentation would be appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be misrepresented as
+*    being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+#pragma once
+#include "orbit/graphics.h"
+
+#if defined(ORB_HAS_OPENGL)
+
+#include <string_view>
+
+#if defined(ORB_OS_WINDOWS)
+#include <windows.h>
+#include <gl/GL.h>
+#include <gl/GLext.h>
+#endif
+
+namespace orb
+{
+namespace gl
+{
+
+enum class buffer_target : GLenum
+{
+	Array             = 0x8892,
+	AtomicCounter     = 0x92c0,
+	CopyRead          = 0x8f36,
+	CopyWrite         = 0x8f37,
+	DispatchIndirect  = 0x90ee,
+	DrawIndirect      = 0x8f3f,
+	ElementArray      = 0x8893,
+	PixelPack         = 0x88eb,
+	PixelUnpack       = 0x88ec,
+	Query             = 0x9192,
+	ShaderStorage     = 0x90d2,
+	Texture           = 0x8c2a,
+	TransformFeedback = 0x8c8e,
+	Uniform           = 0x8a11,
+};
+
+enum class buffer_usage : GLenum
+{
+	StreamDraw  = 0x88e0,
+	StreamRead  = 0x88e1,
+	StreamCopy  = 0x88e2,
+	StaticDraw  = 0x88e4,
+	StaticRead  = 0x88e5,
+	StaticCopy  = 0x88e6,
+	DynamicDraw = 0x88e8,
+	DynamicRead = 0x88e9,
+	DynamicCopy = 0x88ea,
+};
+
+enum class draw_mode : GLenum
+{
+	Points                 = 0x0,
+	LineStrip              = 0x3,
+	LineLoop               = 0x2,
+	Lines                  = 0x1,
+	LineStripAdjacency     = 0xb,
+	LinesAdjacency         = 0xa,
+	TriangleStrip          = 0x5,
+	TriangleFan            = 0x6,
+	Triangles              = 0x4,
+	TriangleStripAdjacency = 0xd,
+	TriangleSAdjacency     = 0xc,
+	Patches                = 0xe,
+};
+
+enum class index_type : GLenum
+{
+	Byte  = 0x1401,
+	Short = 0x1403,
+	Int   = 0x1405,
+};
+
+enum class buffer_param : GLenum
+{
+	Access = 0x88bb,
+	Mapped = 0x88bc,
+	Size   = 0x8764,
+	Usage  = 0x8765,
+};
+
+enum class buffer_pointer_param : GLenum
+{
+	MapPointer = 0x88Bd,
+};
+
+enum class vertex_attrib_array_param : GLenum
+{
+	BufferBinding = 0x889f,
+	Enabled       = 0x8622,
+	Size          = 0x8623,
+	Stride        = 0x8624,
+	Type          = 0x8625,
+	Normalized    = 0x886a,
+	CurrentAttrib = 0x8626,
+};
+
+enum class vertex_attrib_array_pointer_param : GLenum
+{
+	Pointer = 0x8645,
+};
+
+enum class vertex_attrib_data_type : GLenum
+{
+	Byte          = 0x1400,
+	UnsignedByte  = 0x1401,
+	Short         = 0x1402,
+	UnsignedShort = 0x1403,
+	Fixed         = 0x140c,
+	Float         = 0x1406,
+};
+
+extern void (*bind_buffer)(buffer_target target, GLuint buffer);
+extern void (*buffer_data)(buffer_target target, GLsizeiptr size, const GLvoid* data, buffer_usage usage);
+extern void (*buffer_sub_data)(buffer_target target, GLintptr offset, GLsizeiptr size, const GLvoid* data);
+extern void (*delete_buffers)(GLsizei n, const GLuint* buffers);
+extern void (*disable_vertex_attrib_array)(GLuint index);
+extern void (*draw_arrays)(draw_mode mode, GLint first, GLsizei count);
+extern void (*draw_elements)(draw_mode, GLsizei count, index_type type, const GLvoid* indices);
+extern void (*enable_vertex_attrib_array)(GLuint index);
+extern void (*gen_buffers)(GLsizei n, GLuint* buffers);
+extern void (*get_buffer_parameteriv)(buffer_target target, buffer_param value, GLint* data);
+extern void (*get_buffer_pointerv)(buffer_target target, buffer_pointer_param pname, GLvoid** params);
+extern void (*get_vertex_attribdv)(GLuint index, vertex_attrib_array_param pname, GLdouble* params);
+extern void (*get_vertex_attribfv)(GLuint index, vertex_attrib_array_param pname, GLfloat* params);
+extern void (*get_vertex_attribiv)(GLuint index, vertex_attrib_array_param pname, GLint* params);
+extern void (*get_vertex_attrib_pointerv)(GLuint index, vertex_attrib_array_pointer_param pname, GLvoid** pointer);
+extern GLboolean (*is_buffer)(GLuint buffer);
+extern void (*vertex_attrib1f)(GLuint index, GLfloat v0);
+extern void (*vertex_attrib2f)(GLuint index, GLfloat v0, GLfloat v1);
+extern void (*vertex_attrib3f)(GLuint index, GLfloat v0, GLfloat v1, GLfloat v2);
+extern void (*vertex_attrib4f)(GLuint index, GLfloat v0, GLfloat v1, GLfloat v2, GLfloat v3);
+extern void (*vertex_attrib_pointer)(GLuint index, GLint size, vertex_attrib_data_type type, GLboolean normalized, GLsizei stride, const GLvoid* pointer);
+
+namespace this_platform
+{
+
+extern void* get_proc_address(std::string_view name);
+
+}
+
+}
+}
+
+#endif
