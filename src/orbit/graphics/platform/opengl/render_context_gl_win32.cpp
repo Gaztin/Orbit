b40d809d7d1fff1df7a74cca1640c52ab526a0ba
Sebastian Kylander <gaztinerag@gmail.com>
2019-03-03 17:47:14 +0100

    Use 'wglCreateContext' if extension functions are missing

--- src/orbit/graphics/platform/opengl/render_context_gl_win32.cpp
+++ src/orbit/graphics/platform/opengl/render_context_gl_win32.cpp
@@ -91,41 +91,48 @@ render_context_gl::render_context_gl(const window_handle& wh, gl::version v)
 	using wglChoosePixelFormatARB_t    = BOOL (WINAPI*)(HDC hdc, const int* iAttribs, const FLOAT* fAttribs, UINT maxFormats, int* formats, UINT* numFormats);
 	using wglCreateContextAttribsARB_t = HGLRC(WINAPI*)(HDC hdc, HGLRC shareContext, const int* attribs);
 
-	wglChoosePixelFormatARB_t wglChoosePixelFormatARB = (wglChoosePixelFormatARB_t)wglGetProcAddress("wglChoosePixelFormatARB");
+	wglChoosePixelFormatARB_t    wglChoosePixelFormatARB    = (wglChoosePixelFormatARB_t)wglGetProcAddress("wglChoosePixelFormatARB");
 	wglCreateContextAttribsARB_t wglCreateContextAttribsARB = (wglCreateContextAttribsARB_t)wglGetProcAddress("wglCreateContextAttribsARB");
 
-	const int formatAttributes[] =
+	if (wglChoosePixelFormatARB && wglCreateContextAttribsARB)
 	{
-		0x2010, 1,      // WGL_SUPPORT_OPENGL_ARB
-		0x2001, 1,      // WGL_DRAW_TO_WINDOW_ARB
-		0x2002, 1,      // WGL_DRAW_TO_BITMAP_ARB
-		0x2011, 1,      // WGL_DOUBLE_BUFFER_ARB
-		0x2006, 1,      // WGL_SWAP_LAYER_BUFFERS_ARB
-		0x2014, 24,     // WGL_COLOR_BITS_ARB
-		0x2015, 8,      // WGL_RED_BITS_ARB
-		0x2017, 8,      // WGL_GREEN_BITS_ARB
-		0x2019, 8,      // WGL_BLUE_BITS_ARB
-		0x201B, 0,      // WGL_ALPHA_BITS_ARB
-		0x2022, 32,     // WGL_DEPTH_BITS_ARB
-		0x2023, 8,      // WGL_STENCIL_BITS_ARB
-		0x2003, 0x2027, // WGL_ACCELERATION_ARB = WGL_FULL_ACCELERATION_ARB
-		0x2013, 0x202B, // WGL_PIXEL_TYPE_ARB = WGL_TYPE_RGBA_ARB
-		0
-	};
+		const int formatAttributes[] =
+		{
+			0x2010, 1,      // WGL_SUPPORT_OPENGL_ARB
+			0x2001, 1,      // WGL_DRAW_TO_WINDOW_ARB
+			0x2002, 1,      // WGL_DRAW_TO_BITMAP_ARB
+			0x2011, 1,      // WGL_DOUBLE_BUFFER_ARB
+			0x2006, 1,      // WGL_SWAP_LAYER_BUFFERS_ARB
+			0x2014, 24,     // WGL_COLOR_BITS_ARB
+			0x2015, 8,      // WGL_RED_BITS_ARB
+			0x2017, 8,      // WGL_GREEN_BITS_ARB
+			0x2019, 8,      // WGL_BLUE_BITS_ARB
+			0x201B, 0,      // WGL_ALPHA_BITS_ARB
+			0x2022, 32,     // WGL_DEPTH_BITS_ARB
+			0x2023, 8,      // WGL_STENCIL_BITS_ARB
+			0x2003, 0x2027, // WGL_ACCELERATION_ARB = WGL_FULL_ACCELERATION_ARB
+			0x2013, 0x202B, // WGL_PIXEL_TYPE_ARB = WGL_TYPE_RGBA_ARB
+			0
+		};
 
-	int pixelFormats = 0;
-	UINT pixelFormatCount = 0;
-	wglChoosePixelFormatARB(m_hdc, formatAttributes, nullptr, 1, &pixelFormats, &pixelFormatCount);
+		int pixelFormats = 0;
+		UINT pixelFormatCount = 0;
+		wglChoosePixelFormatARB(m_hdc, formatAttributes, nullptr, 1, &pixelFormats, &pixelFormatCount);
 
-	const int contextAttributes[] =
+		const int contextAttributes[] =
+		{
+			0x2091, get_context_version_major(v), // WGL_CONTEXT_MAJOR_VERSION_ARB
+			0x2092, get_context_version_minor(v), // WGL_CONTEXT_MINOR_VERSION_ARB
+			0x9126, 0x00000001,                   // WGL_CONTEXT_PROFILE_MASK_ARB = WGL_CONTEXT_CORE_PROFILE_BIT_ARB
+			0
+		};
+
+		m_hglrc = wglCreateContextAttribsARB(m_hdc, nullptr, contextAttributes);
+	}
+	else
 	{
-		0x2091, get_context_version_major(v), // WGL_CONTEXT_MAJOR_VERSION_ARB
-		0x2092, get_context_version_minor(v), // WGL_CONTEXT_MINOR_VERSION_ARB
-		0x9126, 0x00000001,                   // WGL_CONTEXT_PROFILE_MASK_ARB = WGL_CONTEXT_CORE_PROFILE_BIT_ARB
-		0
-	};
-
-	m_hglrc = wglCreateContextAttribsARB(m_hdc, nullptr, contextAttributes);
+		m_hglrc = wglCreateContext(m_hdc);
+	}
 
 	make_current();
 	m_functions = gl::load_functions();
