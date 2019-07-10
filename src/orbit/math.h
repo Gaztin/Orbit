508c1c680e1ac7207a8aba2078ac9468d72e9f2e
Sebastian Kylander <gaztinerag@gmail.com>
2019-07-10 23:47:45 +0200

    Reformat math common header

--- src/orbit/math.h
+++ src/orbit/math.h
@@ -18,18 +18,16 @@
 #pragma once
 #include "orbit/core.h"
 
-#if defined(ORB_BUILD_MATH)
-#define ORB_API_MATH ORB_DLL_EXPORT
+#if defined( ORB_BUILD_MATH )
+#  define ORB_API_MATH ORB_DLL_EXPORT
 #else
-#define ORB_API_MATH ORB_DLL_IMPORT
+#  define ORB_API_MATH ORB_DLL_IMPORT
 #endif
 
 namespace orb
 {
-
-constexpr float Pi                 = 3.141592741f;
-constexpr float E                  = 2.718281746f;
-constexpr float PythagorasConstant = 1.414213538f;
-constexpr float GoldenRatio        = 1.618034005f;
-
+	constexpr float Pi                 = 3.141592741f;
+	constexpr float E                  = 2.718281746f;
+	constexpr float PythagorasConstant = 1.414213538f;
+	constexpr float GoldenRatio        = 1.618034005f;
 }
