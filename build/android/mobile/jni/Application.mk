bc1d384890100b026acad0803ae93e1f2888afd6
Sebastian Kylander <gaztinerag@gmail.com>
2019-07-18 09:31:50 +0200

    Remove old build files

--- build/android/mobile/jni/Application.mk
+++
@@ -1,2 +0,0 @@
-APP_BUILD_SCRIPT := Android.mk
-APP_STL          := c++_shared
--- build/android/wear/src/main/java/com/gaztin/orbit/MainApplication.java
+++
@@ -1,7 +0,0 @@
-package com.gaztin.orbit;
-
-import android.app.Application;
-
-public class MainApplication extends Application
-{
-}
