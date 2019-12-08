3045bef6413b45e16dd66a2b765e8984423cf38e
Sebastian Kylander <developer@gaztin.com>
2019-12-08 22:56:43 +0100

    Add model sample

---
+++ src/Samples/03-Model/Android/java/com/gaztin/orbit/samples/cube/MainActivity.java
@@ -0,0 +1,11 @@
+package com.gaztin.orbit.samples.model;
+
+import android.app.NativeActivity;
+
+public class MainActivity extends NativeActivity
+{
+	static
+	{
+		System.loadLibrary( "03-Model" );
+	}
+}
