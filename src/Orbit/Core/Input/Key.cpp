30b728b913ce70f5f112d4d0b796a92a182ebdb3
Gaztin <gaztinerag@gmail.com>
2019-12-18 19:50:55 +0100

    Add conversion from Xlib keysyms

--- src/Orbit/Core/Input/Key.cpp
+++ src/Orbit/Core/Input/Key.cpp
@@ -17,8 +17,12 @@
 
 #include "Key.h"
 
+#include "Orbit/Core/Widget/Window.h"
+
 #if defined( ORB_OS_WINDOWS )
 #  include <Windows.h>
+#elif defined( ORB_OS_LINUX )
+#  include <X11/keysym.h>
 #endif
 
 ORB_NAMESPACE_BEGIN
@@ -113,6 +117,107 @@ Key ConvertSystemKey( uint32_t system_key )
 		case VK_UP:       return Key::ArrowUp;
 	}
 
+#elif defined( ORB_OS_LINUX )
+
+	Key key = Key::Unknown;
+
+	if( Window* window = Window::GetPtr(); window )
+	{
+		int     keysyms_per_keycode_return;
+		KeySym* keysym = XGetKeyboardMapping( window->GetPrivateDetails().display, system_key, 1, &keysyms_per_keycode_return );
+		
+		if( keysym )
+		{
+			switch( keysym[ 0 ] )
+			{
+				case XK_0: { key = Key::_0; } break;
+				case XK_1: { key = Key::_1; } break;
+				case XK_2: { key = Key::_2; } break;
+				case XK_3: { key = Key::_3; } break;
+				case XK_4: { key = Key::_4; } break;
+				case XK_5: { key = Key::_5; } break;
+				case XK_6: { key = Key::_6; } break;
+				case XK_7: { key = Key::_7; } break;
+				case XK_8: { key = Key::_8; } break;
+				case XK_9: { key = Key::_9; } break;
+
+				case XK_a: { key = Key::A; } break;
+				case XK_b: { key = Key::B; } break;
+				case XK_c: { key = Key::C; } break;
+				case XK_d: { key = Key::D; } break;
+				case XK_e: { key = Key::E; } break;
+				case XK_f: { key = Key::F; } break;
+				case XK_g: { key = Key::G; } break;
+				case XK_h: { key = Key::H; } break;
+				case XK_i: { key = Key::I; } break;
+				case XK_j: { key = Key::J; } break;
+				case XK_k: { key = Key::K; } break;
+				case XK_l: { key = Key::L; } break;
+				case XK_m: { key = Key::M; } break;
+				case XK_n: { key = Key::N; } break;
+				case XK_o: { key = Key::O; } break;
+				case XK_p: { key = Key::P; } break;
+				case XK_q: { key = Key::Q; } break;
+				case XK_r: { key = Key::R; } break;
+				case XK_s: { key = Key::S; } break;
+				case XK_t: { key = Key::T; } break;
+				case XK_u: { key = Key::U; } break;
+				case XK_v: { key = Key::V; } break;
+				case XK_w: { key = Key::W; } break;
+				case XK_x: { key = Key::X; } break;
+				case XK_y: { key = Key::Y; } break;
+				case XK_z: { key = Key::Z; } break;
+
+				case XK_F1:  { key = Key::F1;  } break;
+				case XK_F2:  { key = Key::F2;  } break;
+				case XK_F3:  { key = Key::F3;  } break;
+				case XK_F4:  { key = Key::F4;  } break;
+				case XK_F5:  { key = Key::F5;  } break;
+				case XK_F6:  { key = Key::F6;  } break;
+				case XK_F7:  { key = Key::F7;  } break;
+				case XK_F8:  { key = Key::F8;  } break;
+				case XK_F9:  { key = Key::F9;  } break;
+				case XK_F10: { key = Key::F10; } break;
+				case XK_F11: { key = Key::F11; } break;
+				case XK_F12: { key = Key::F12; } break;
+
+				case XK_Escape:      { key = Key::Escape;       } break;
+				case XK_Tab:         { key = Key::Tab;          } break;
+				case XK_Caps_Lock:   { key = Key::CapsLock;     } break;
+				case XK_Shift_L:     { key = Key::Shift;        } break;
+				case XK_Shift_R:     { key = Key::Shift;        } break;
+				case XK_Control_L:   { key = Key::Control;      } break;
+				case XK_Control_R:   { key = Key::Control;      } break;
+				case XK_Alt_L:       { key = Key::Alt;          } break;
+				case XK_Alt_R:       { key = Key::Alt;          } break;
+				case XK_space:       { key = Key::Space;        } break;
+				case XK_Return:      { key = Key::Return;       } break;
+				case XK_BackSpace:   { key = Key::Backspace;    } break;
+				case XK_Print:       { key = Key::PrintScreen;  } break;
+				case XK_Scroll_Lock: { key = Key::ScrollLock;   } break;
+				case XK_Pause:       { key = Key::Pause;        } break;
+				case XK_Insert:      { key = Key::Insert;       } break;
+				case XK_Home:        { key = Key::Home;         } break;
+				case XK_Page_Up:     { key = Key::PageUp;       } break;
+				case XK_Delete:      { key = Key::Delete;       } break;
+				case XK_End:         { key = Key::End;          } break;
+				case XK_Page_Down:   { key = Key::PageDown;     } break;
+				case XK_Num_Lock:    { key = Key::NumLock;      } break;
+				case XK_KP_Divide:   { key = Key::Divide;       } break;
+				case XK_KP_Multiply: { key = Key::Multiply;     } break;
+				case XK_KP_Subtract: { key = Key::Subtract;     } break;
+				case XK_KP_Add:      { key = Key::Add;          } break;
+				case XK_KP_Decimal:  { key = Key::DecimalPoint; } break;
+				case XK_Down:        { key = Key::ArrowDown;    } break;
+				case XK_Left:        { key = Key::ArrowLeft;    } break;
+				case XK_Right:       { key = Key::ArrowRight;   } break;
+				case XK_Up:          { key = Key::ArrowUp;      } break;
+			}
+		}
+	}
+	
+	return key;
+	
 #endif
 
 }
