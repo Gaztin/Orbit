f99de298bd9a005f99d10e37a54ce5c3d660ee7e
Sebastian Kylander <developer@gaztin.com>
2020-02-02 23:33:59 +0100

    Add helper function to copy span data

--- src/Orbit/Core/Utility/Span.h
+++ src/Orbit/Core/Utility/Span.h
@@ -67,8 +67,20 @@ public:
 
 public:
 
-	const T* begin( void ) const { return m_ptr; }
-	const T* end  ( void ) const { return ( m_ptr + m_count ); }
+	size_t GetSize( void ) const { return m_count; }
+
+	std::unique_ptr< T[] > Copy( void ) const
+	{
+		std::unique_ptr< T[] > ptr( new T[ m_count ] );
+		std::copy( begin(), end(), &ptr[ 0 ] );
+
+		return ptr;
+	}
+
+public:
+
+	const T* begin  ( void ) const { return m_ptr; }
+	const T* end    ( void ) const { return ( m_ptr + m_count ); }
 
 private:
 
